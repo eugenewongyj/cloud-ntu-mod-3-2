639a68438489d7af95f4dc5d51abfdc20cbe0701
eugenewongyj <eugenewongyj@gmail.com>
2026-09-23 20:33:39 +0800

    add code

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket       = "sctp-tfstate-ce13"
+    key          = "eugene/mod-3-2/terraform.tfstate"
+    region       = "us-east-1"
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "workshop" {
+  bucket_prefix = "eugene-s3-mod-3-2"
+}
